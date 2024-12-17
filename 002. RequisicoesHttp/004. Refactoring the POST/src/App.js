d8be3276b34ee6ad1228199ea1649461c29d4bd2
Lucas Carvalho <95986277+LucasCarvalho23@users.noreply.github.com>
2024-12-17 17:53:16 -0300

    Refactoring the POST
    Refactoring the POST

---
+++ 002. RequisicoesHttp/004. Refactoring the POST/src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import {useState, useEffect} from "react";
+import { useFetch } from './hooks/useFetch';
+
+const url = "http://localhost:3000/products";
+
+function App() {
+  
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+  const {data: items, httpConfig} = useFetch(url)
+
+  const handleSubmit = async (e)=> {
+    e.preventDefault()
+    const product = {name, price}
+    httpConfig(product, "POST")
+    setName("")
+    setPrice("")
+    
+  }
+
+  return (
+    <div className="App">
+
+      <h1>Product List</h1>
+
+      <ul>
+        {items && items.map( (products)=> (
+          <li key={products.id}>{products.name} = R$ {products.price}</li>
+        ))}
+      </ul>
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+        <label>
+            Nome: 
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)}/>
+          </label>
+          <label>
+            Preço: 
+            <input type="text" value={price} name="price" onChange={(e) => setPrice(e.target.value)}/>
+          </label>
+          <input type="submit" value="Criar produto"/>
+        </form>
+      </div>
+
+    </div>
+
+  );
+}
+
+export default App;
